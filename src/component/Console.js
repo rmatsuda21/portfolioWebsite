40e752e13b5b770ffc81e67027512fc4764b7d6e
rmatsuda <reo.matsuda@gmail.com>
2021-07-19 20:26:48 -0500

    Updated homepage + mobile support

---
+++ src/component/Console.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+// Usage
+export function Console() {
+
+    // Call our hook for each key that we'd like to monitor
+    const [text, setText] = useState("");
+    const [pos, setPos] = useState(0);
+
+    const [sText, setSText] = useState("");
+    const [cText, setCText] = useState(" ");
+    const [nText, setNText] = useState("");
+
+    function updateText(text, pos){
+        var s, c, n;
+        s = (text.slice(0,pos));
+        if(pos < text.length)
+            c = (text.slice(pos, pos+1));
+        else
+            c = (' ');
+        if(pos < text.length - 1)
+            n = (text.slice(pos+1, text.length));
+        else
+            n = ('');
+
+        // console.log(pos);
+        // console.log('S:' + s + ' | C:' + c + ' | N:' + n);
+
+        setSText(s);
+        setCText(c);
+        setNText(n);
+    }
+
+    function addChar(c) {
+        var newText = text.slice(0,pos) + c + text.slice(pos);
+        var newPos = pos + 1;
+        setText(newText);
+        setPos(newPos);
+        
+        updateText(newText, newPos);
+    }
+
+    const downHandler = ({ key }) => {
+        console.log(key);
+        if(key === 'ArrowLeft') {
+            if(pos > 0) {
+                setPos(pos - 1);
+                updateText(text, pos - 1);
+            }
+        }
+        else if(key === 'ArrowRight') {
+            if(pos < text.length) {
+                setPos(pos + 1);
+                updateText(text, pos + 1);
+            }
+        }
+        else if(key === 'Backspace') {
+            if(pos > 0) {
+                var newText = text.slice(0,pos-1) + text.slice(pos);
+                var newPos = pos - 1;
+                setText(newText);
+                setPos(newPos);
+                
+                updateText(newText, newPos);
+            }
+        }
+        else if(key.length === 1 && /[0-9a-zA-Z{ }]/.test(key)){
+            addChar(key);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", downHandler);
+        return () => {
+            window.removeEventListener("keydown", downHandler);
+        };
+    });
+
+    return (
+        <div style={{whiteSpace:'pre'}}>
+            ><span>{sText}</span><span style={{backgroundColor:'white',color:'black'}}>{cText}</span><span>{nText}</span>
+        </div>
+    );
+}
--- src/component/home.js
+++ src/component/home.js
@@ -1,41 +1,43 @@
 import React, { Component } from 'react';
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+  } from "react-device-detect";
 
 import '../stylesheets/home.css';
+import { Console } from './Console';
 
-import Cloud from './cloud';
-import Star from './star';
+// import Star from './star';
 
-function Stars() {
-    var stars = []
-    for(let i=0; i<(Math.floor(Math.random() * 10) + 50); ++i) {
-        var t = Math.floor(Math.random() * 80) + 5;
-        var l = Math.floor(Math.random() * 90) + 5;
-        var w = Math.floor(Math.random() * 3) + 5
-        var c = Math.round(Math.random());
-        var col = c ? 'var(--contrast)' : 'var(--highlight)';
-        var stroke = c ? 'var(--highlight)' : 'var(--contrast)';
-        stars.push(<Star key={i} fill={col} width={w+'vw'} position='absolute' top={t+'vh'} left={l+'vw'} bottom='' right=''/>)
-    }
-    return stars;
-}
+// function Stars() {
+//     var stars = []
+//     for(let i=0; i<(Math.floor(Math.random() * 10) + 50); ++i) {
+//         var t = Math.floor(Math.random() * 80) + 5;
+//         var l = Math.floor(Math.random() * 90) + 5;
+//         var w = Math.floor(Math.random() * 3) + 5
+//         var c = Math.round(Math.random());
+//         var col = c ? 'var(--contrast)' : 'var(--highlight)';
+//         var stroke = c ? 'var(--highlight)' : 'var(--contrast)';
+//         stars.push(<Star key={i} fill={col} width={w+'vw'} position='absolute' top={t+'vh'} left={l+'vw'} bottom='' right=''/>)
+//     }
+//     return stars;
+// }
 
 class Home extends Component {
     render() {
         return (
-        <div className="mainContent">
-            {/* <p>
-            &nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;__<br/>
-            |&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;/__&nbsp;&nbsp;/&nbsp;/________&nbsp;&nbsp;____&nbsp;___&nbsp;&nbsp;___&nbsp;<br/>
-            |&nbsp;|&nbsp;/|&nbsp;/&nbsp;/&nbsp;_&nbsp;\/&nbsp;/&nbsp;___/&nbsp;__&nbsp;\/&nbsp;__&nbsp;`__&nbsp;\/&nbsp;_&nbsp;\<br/>
-            |&nbsp;|/&nbsp;|/&nbsp;/&nbsp;&nbsp;__/&nbsp;/&nbsp;/__/&nbsp;/_/&nbsp;/&nbsp;/&nbsp;/&nbsp;/&nbsp;/&nbsp;/&nbsp;&nbsp;__/<br/>
-            |__/|__/\___/_/\___/\____/_/&nbsp;/_/&nbsp;/_/\___/<br/>                     
-            </p> */}
-            <p>
-            ██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;███████&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██████&nbsp;&nbsp;██████&nbsp;&nbsp;███&nbsp;&nbsp;&nbsp;&nbsp;███&nbsp;███████<br/>
-            ██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;████&nbsp;&nbsp;████&nbsp;██<br/>
-            ██&nbsp;&nbsp;█&nbsp;&nbsp;██&nbsp;█████&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;██&nbsp;████&nbsp;██&nbsp;█████<br/>
-            ██&nbsp;███&nbsp;██&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;██&nbsp;&nbsp;██&nbsp;&nbsp;██&nbsp;██<br/>
-            &nbsp;███&nbsp;███&nbsp;&nbsp;███████&nbsp;███████&nbsp;&nbsp;██████&nbsp;&nbsp;██████&nbsp;&nbsp;██&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;██&nbsp;███████<br/>
+        <>
+        <BrowserView>
+        <div className="mainContent" id="mainContainer">
+            <p style={{whiteSpace:'pre', lineHeight:'99%'}}>
+            ██╗  ██╗ ██████╗ ██╗    ██╗██████╗ ██╗   ██╗<br/>
+            ██║  ██║██╔═══██╗██║    ██║██╔══██╗╚██╗ ██╔╝<br/>
+            ███████║██║   ██║██║ █╗ ██║██║  ██║ ╚████╔╝ <br/>
+            ██╔══██║██║   ██║██║███╗██║██║  ██║  ╚██╔╝  <br/>
+            ██║  ██║╚██████╔╝╚███╔███╔╝██████╔╝   ██║   <br/>
+            ╚═╝  ╚═╝ ╚═════╝  ╚══╝╚══╝ ╚═════╝    ╚═╝   <br/>
             </p>
             <h3>This site is still WIP, please check back later</h3>
             <p>
@@ -43,17 +45,15 @@ class Home extends Component {
                 ______________________________<br/><br/><br/>
                 ¯\_(^^)_/¯<br/>
             </p>
-            {/* <Cloud fill='var(--secondary)' width='25vw' position='absolute' top='' left='48vw' bottom='-1vw' right='' stroke='var(--contrast)'/>
-            <Cloud fill='var(--secondary)' width='30vw' position='absolute' top='' left='' bottom='-3vw' right='49vw' stroke='var(--contrast)'/>
-            <Cloud fill='var(--secondary)' width='60vw' position='absolute' top='' left='-25vw' bottom='-10vw' right='' stroke='var(--contrast)'/>
-            <Cloud fill='var(--secondary)' width='50vw' position='absolute' top='' left='' bottom='-10vw' right='-15vw' stroke='var(--contrast)'/>
-            <Stars/> */}
-            {/* <Star fill='var(--secondary)' width='8vw' position='absolute' top='15vw' left='' bottom='' right='15vw' stroke='var(--contrast)'/>
-            <Star fill='var(--secondary)' width='6vw' position='absolute' top='25vw' left='' bottom='' right='15vw' stroke='var(--contrast)'/>
-            <Star fill='var(--secondary)' width='7vw' position='absolute' top='35vw' left='' bottom='' right='15vw' stroke='var(--contrast)'/>
-            <Star fill='var(--secondary)' width='8vw' position='absolute' top='19vw' left='' bottom='' right='15vw' stroke='var(--contrast)'/>
-            <Star fill='var(--secondary)' width='6vw' position='absolute' top='22vw' left='' bottom='' right='15vw' stroke='var(--contrast)'/> */}
+            <Console/>
         </div>
+        </BrowserView>
+        <MobileView>
+        <div className="mainContent" id="mainContainer">
+            <h1>IS MOBILE</h1>
+        </div>
+        </MobileView>
+        </>
         );
     }
 }
